26216d2502a6a15bfaa7367769da82514711c9a2
Shivaansh Dhingra <dhingrashivaansh@gmail.com>
2025-01-03 17:46:22 -0500

    fixing adduser and improving addjob

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://job-application-app-nine.vercel.app',
-    'http://localhost:3000',
-    'https://job-application-app-nine.vercel.app/'
+    'http://localhost:3000'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
